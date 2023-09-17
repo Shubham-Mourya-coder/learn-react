5b9626ee9ca261656ae8b8a42c0d2f0ee3f3425d
Shubham-Mourya-coder <shubhammourya148@gmail.com>
2023-09-17 11:54:03 +0530

    first commit

---
+++ react.js
@@ -0,0 +1,9 @@
+
+const heading = React.createElement("h1",{ id:"heading1"},"Hello World from Reacr!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(heading);
+
+root.render(heading);
+   
